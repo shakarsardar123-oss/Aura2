4b6c42ad4dcf8c7d956e1346db10a5141af4426a
shakarsardar123-oss <shakarsardar123@gmail.com>
2026-09-17 18:46:55 +0300

    Full clean fix for Result and MemoryFailure imports

--- lib/features/semantic_memory/domain/models/memory_failure.dart
+++ lib/features/semantic_memory/domain/models/memory_failure.dart
@@ -1,7 +1,4 @@
 import 'package:aura_assistant/core/errors/failure.dart';
-import 'package:aura_assistant/core/errors/result.dart';
-
-export 'package:aura_assistant/core/errors/result.dart';
 
 sealed class MemoryFailure extends Failure {
   const MemoryFailure._(super.message, [super.cause]);
--- lib/features/semantic_memory/domain/services/memory_storage_service.dart
+++ lib/features/semantic_memory/domain/services/memory_storage_service.dart
@@ -1,3 +1,4 @@
+import 'package:aura_assistant/core/errors/result.dart';
 import '../models/memory_entry.dart';
 import '../models/memory_failure.dart';
 import '../models/memory_type.dart';
